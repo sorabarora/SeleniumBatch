71c1cb4ca5b1a1c36e19caa2767401710cd5cd6f
manu <manu@DESKTOP-FGJUHHM>
2023-07-31 19:54:17 +0530

    Third Commit Done

---
+++ Assignment_1/src/s0/Constructorcall1.java
@@ -0,0 +1,35 @@
+package s0;
+
+public class Constructorcall1 {
+	
+	int a,b,c,d,e;
+	
+	public Constructorcall1(int f,int g,int h,int i,int j)
+	{
+		a=f;
+		b=g;
+		c=h;
+		d=i;
+		e=j;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Constructorcall1 cc1=new Constructorcall1(10,20,30,40,50);
+		System.out.println(cc1.a);
+		System.out.println(cc1.b);
+		System.out.println(cc1.c);
+		System.out.println(cc1.d);
+		System.out.println(cc1.e);
+		
+		
+		
+		
+		
+
+	}
+}
+
+
+
